9f4c808b151f469dd0bb3e40547b37ad011f9c90
Luiza Campos <luiza.almcampos@gmail.com>
2021-02-17 14:25:42 -0300

    função search adicionada

--- src/App.js
+++ src/App.js
@@ -6,12 +6,49 @@ import logo from './images/logo.jpg'
 
 import { FaGithub } from 'react-icons/fa';  
 import { FaLinkedin } from 'react-icons/fa'
+import { FaSearch } from 'react-icons/fa'
+import { FaArrowLeft } from 'react-icons/fa'
 
 function App() {
 
   const [people, setPeople] = useState([])
   const [next, setNext] = useState('')
   const [page, setPage] = useState(1)
+  const [searchedPeople, setSearchedPeople] = useState([])
+  const [search, setSearch] = useState(null)
+  const [searching, setSearching] = useState('')
+
+  function getSearch(e) {
+    setSearching(e.target.value)
+    console.log(searching)
+
+    setSearch(searching)
+    console.log(search)
+
+    axios
+      .get(`https://swapi.dev/api/people/?search=${search}`)
+      .then(response => {
+        console.log(response.data)
+
+        setSearchedPeople(response.data.results)
+      })
+  }
+
+  function doSearch(e) {
+    e.preventDefault()
+    axios
+      .get(`https://swapi.dev/api/people/?search=${search}`)
+      .then(response => {
+        console.log(response.data)
+
+        setSearchedPeople(response.data.results)
+      })
+  }
+
+  function clearSearch() {
+    setSearching('')
+    setSearch(null)
+  }
 
   function loadPeople(page = 1) {
     axios
@@ -50,11 +87,37 @@ function App() {
     <>
       <div className="App">
           <header>
-                  <img src={logo} alt="Stars Wars" />
+                  <a href="/"><img src={logo} alt="Stars Wars" /></a>
           </header>
-        
-          <div className="container">
-              {people.map(person => {
+          <form onSubmit={doSearch}>
+            <input onChange={getSearch} type="text" placeholder="Pesquise por um personagem"></input>
+            <button className="SearchBtn"><FaSearch /></button>
+          </form>
+          {search && 
+            <>
+              <span onClick={clearSearch}><FaArrowLeft /> Voltar para todos os personagens</span> 
+              <div className="container">
+                {searchedPeople.map(searchedPerson => {
+                  return (
+                    <div className="card" key={searchedPerson.name}>
+                        <h2>{searchedPerson.name}</h2>
+                        <p><strong>Height:</strong> {searchedPerson.height} cm</p>
+                        <p><strong>Mass:</strong> {searchedPerson.mass} kg</p>
+                        <p><strong>Hair color:</strong> {searchedPerson.hair_color}</p>
+                        <p><strong>Skin color:</strong> {searchedPerson.skin_color}</p>
+                        <p><strong>Eye color:</strong> {searchedPerson.eye_color}</p>
+                        <p><strong>Birth year:</strong> {searchedPerson.birth_year}</p>
+                        <p><strong>Gender:</strong> {searchedPerson.gender}</p>
+                      </div>
+                  )
+                })}
+              </div> 
+            </>
+            }
+    
+            {!search && 
+              <div className="container">
+                {people.map(person => {
                   return <div className="card" key={person.name}>
                               <h2>{person.name}</h2>
                               <p><strong>Height:</strong> {person.height} cm</p>
@@ -65,11 +128,11 @@ function App() {
                               <p><strong>Birth year:</strong> {person.birth_year}</p>
                               <p><strong>Gender:</strong> {person.gender}</p>
                           </div>
-              })}
-          </div>
+                })}
+              </div>}
           <div className="btn-container">
                   <button className="btn" disabled={page === 1} onClick={prevPage}>Prev</button>
-                  <button className="btn" disabled={next === null} onClick={nextPage}>Next</button>
+                  <button className="btn" disabled={next === null || search} onClick={nextPage}>Next</button>
           </div>
 
         <footer>
